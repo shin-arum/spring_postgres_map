447139e755abafffb6594fe97d5097906b72a3d5
arumshin <96033117+parkmisuk@users.noreply.github.com>
2023-02-28 12:09:56 +0900

    스프링 생성

---
+++ src/main/java/com/example/spring_postgres_map/SpringPostgresMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_postgres_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPostgresMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_postgres_map/SpringPostgresMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_postgres_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
